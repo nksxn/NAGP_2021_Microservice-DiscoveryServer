b57aed21e7459be41516d3262793c99a1d4d48fb
nksxn <vibhu.saxena31@gmail.com>
2021-02-28 12:55:10 +0530

    added gitignore file

--- src/main/java/com/nagarro/nagp/discovery/server/DiscoveryServerApplication.java
+++ src/main/java/com/nagarro/nagp/discovery/server/DiscoveryServerApplication.java
@@ -1,4 +1,4 @@
-package org.selflearning.msa.discovery.server;
+package com.nagarro.nagp.discovery.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
